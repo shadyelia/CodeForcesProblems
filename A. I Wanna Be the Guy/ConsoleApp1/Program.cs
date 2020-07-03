609276247608145cc247e623b49d9c2ca657d438
ShadyElia <shady.elia@hotmail.com>
2020-07-03 23:19:06 +0200

    finish A. I Wanna Be the Guy

---
+++ A. I Wanna Be the Guy/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            List<string> pData = Console.ReadLine().Split(' ').ToList();
+            int p = Convert.ToInt32(pData[0]);
+            pData.RemoveAt(0);
+            int[] pArr = pData.Select(int.Parse).ToArray();
+
+            List<string> qData = Console.ReadLine().Split(' ').ToList();
+            int q = Convert.ToInt32(qData[0]);
+            qData.RemoveAt(0);
+            int[] qArr = qData.Select(int.Parse).ToArray();
+
+            string result = "Oh, my keyboard!";
+
+            if (n == p || n == q) result = "I become the guy.";
+            else
+            {
+                HashSet<int> numbers = new HashSet<int>();
+                for (int j = 1; j <= n; j++)
+                {
+                    numbers.Add(j);
+                }
+                int i = 0;
+                while (numbers.Count() > 0)
+                {
+                    if (i >= p && i >= q) break;
+
+                    if (i < p)
+                    {
+                        if (numbers.Contains(pArr[i]))
+                        {
+                            numbers.Remove(pArr[i]);
+                        }
+                    }
+
+                    if (i < q)
+                    {
+                        if (numbers.Contains(qArr[i]))
+                        {
+                            numbers.Remove(qArr[i]);
+                        }
+                    }
+
+                    i++;
+                }
+                if (numbers.Count() == 0) result = "I become the guy.";
+
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
--- B. Taxi/Taxi.py
+++ B. Taxi/Taxi.py
@@ -1,4 +1,3 @@
-import math
 
 
 def main():
@@ -9,34 +8,25 @@ def main():
 
     arrayInt = [int(i) for i in array]
 
-    arrayLen = len(arrayInt)
-    if arrayInt.count(arrayInt[0]) == arrayLen:
-        if arrayInt[0] == 1:
-            numberOfTaxi = math.ceil(arrayLen / 4)
-        elif arrayInt[0] == 2:
-            numberOfTaxi = math.ceil(arrayLen / 2)
-        else:
-            numberOfTaxi = math.ceil(arrayLen)
-    else:
-        while (len(arrayInt) > 0):
-            num = arrayInt[0]
-            del arrayInt[0]
-            remainder = 4 - num
-            i = 0
-            while (remainder > 0 and i < len(arrayInt)):
-                try:
-                    index = arrayInt.index(remainder)
-                    del arrayInt[index]
-                    break
-                except:
-                    num2 = arrayInt[i]
-                    if(num2 < remainder):
-                        remainder = remainder - num2
-                        del arrayInt[i]
-                    else:
-                        i = i+1
+    while (len(arrayInt) > 0):
+        num = arrayInt[0]
+        del arrayInt[0]
+        remainder = 4 - num
+        i = 0
+        while (remainder > 0 and i < len(arrayInt)):
+            try:
+                index = arrayInt.index(remainder)
+                del arrayInt[index]
+                break
+            except:
+                num2 = arrayInt[i]
+                if(num2 < remainder):
+                    remainder = remainder - num2
+                    del arrayInt[i]
+                else:
+                    i = i+1
 
-            numberOfTaxi = numberOfTaxi + 1
+        numberOfTaxi = numberOfTaxi + 1
 
     print(numberOfTaxi)
 
