30e73658d5a1873b9d00a2ff68427b471759c025
ShadyElia <shady.elia@hotmail.com>
2020-07-23 23:20:39 +0200

    add git ignore and solve some problems

---
+++ A. Helpful Maths/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A._Helpful_Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split('+');
+            int[] numbers = Array.ConvertAll(data, s => int.Parse(s));
+
+            Array.Sort(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i]);
+                if (i != numbers.Length - 1) Console.Write('+');
+            }
+        }
+    }
+}
---
+++ A. Word Capitalization/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A._Word_Capitalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+
+            string result = "";
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i].Length > 1)
+                    result += char.ToUpper(data[i][0]) + data[i].Substring(1);
+                else
+                    result += char.ToUpper(data[i][0]);
+
+                if (i != data.Length - 1)
+                    result += " ";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
