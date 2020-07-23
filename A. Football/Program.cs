002cb40e56abcfa78677e3c1290a0bdfb9bf23ad
ShadyElia <shady.elia@hotmail.com>
2020-07-23 23:18:28 +0200

    finish some problems

---
+++ A. Football/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace A._Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            int counter = 1;
+
+            for (int i = 1; i < line.Length; i++)
+            {
+                if (line[i] == line[i - 1])
+                    counter++;
+                else
+                    counter = 1;
+
+                if (counter == 7)
+                {
+                    Console.WriteLine("YES");
+                    break;
+                }
+            }
+            if (counter < 7)
+                Console.WriteLine("NO");
+        }
+    }
+}
