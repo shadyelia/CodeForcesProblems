841979495dd644f597e0a57212cc1938dd408497
ShadyElia <shady.elia@hotmail.com>
2020-07-31 16:09:45 +0200

    solve some problems

---
+++ A. Bear and Big Brother/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A._Bear_and_Big_Brother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(data[0]);
+            int b = Convert.ToInt32(data[1]);
+            int counter = 0;
+
+            while (a <= b)
+            {
+                a *= 3;
+                b *= 2;
+                counter++;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ A. Tram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A._Tram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumberOfPassengers = 0, maxNumberOfPassengers = 0;
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ');
+
+                totalNumberOfPassengers -= Convert.ToInt32(data[0]);
+                totalNumberOfPassengers += Convert.ToInt32(data[1]);
+
+                if (totalNumberOfPassengers > maxNumberOfPassengers)
+                    maxNumberOfPassengers = totalNumberOfPassengers;
+            }
+
+            Console.WriteLine(maxNumberOfPassengers);
+        }
+    }
+}
