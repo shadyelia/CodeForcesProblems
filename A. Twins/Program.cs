8b77dc690d2cd6f26a50aa992fa7c2df009c46a8
ShadyElia <shady.elia@hotmail.com>
2020-07-31 21:52:02 +0200

    solve some problems

---
+++ A. Twins/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace A._Twins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int totalSum = 0;
+            string[] data = Console.ReadLine().Split(' ');
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = Convert.ToInt32(data[i]);
+                numbers.Add(number);
+                totalSum += number;
+            }
+
+            numbers.Sort();
+
+            int sum = 0, counter = 0;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (sum > totalSum)
+                    break;
+
+                sum += numbers[i];
+                totalSum -= numbers[i];
+                counter++;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ A. Wrong Subtraction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A._Wrong_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+
+            long n = Convert.ToInt64(data[0]);
+            int k = Convert.ToInt32(data[1]);
+
+            while (k > 0)
+            {
+                if (n % 10 == 0) n /= 10;
+                else n--;
+
+                k--;
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
