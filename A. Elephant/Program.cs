5ace3c1d998d63f9fc458d7c9b5fc35f9850fe3c
ShadyElia <shady.elia@hotmail.com>
2020-08-01 23:07:34 +0200

    solve some problems

---
+++ A. Elephant/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A._Elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int counter = n / 5;
+            if (n % 5 != 0) counter++;
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ B. Queue at the School/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace B._Queue_at_the_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            short Length = Convert.ToInt16(line[0]);
+            short seconds = Convert.ToInt16(line[1]);
+            string data = Console.ReadLine();
+            string result = data;
+
+            while (seconds != 0)
+            {
+                data = result;
+                result = "";
+
+                for (int i = 0; i < Length - 1; i++)
+                {
+                    if (data[i] == 'B' && data[i + 1] == 'G')
+                    {
+                        result += "GB";
+                        i++;
+                    }
+                    else result += data[i];
+                }
+                if (result.Length < Length) result += data[Length-1];
+
+                if (result == data) break;
+
+                seconds--;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
