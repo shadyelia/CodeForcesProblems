5e44356bd6d84c1dec430b899ceda346ea00e89a
ShadyElia <shady.elia@hotmail.com>
2020-08-08 14:53:30 +0200

    solve some problems

---
+++ A. Anton and Danik/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace A._Anton_and_Danik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            string line = Console.ReadLine();
+            long numberOFA = 0, numberOFB = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 'A') numberOFA++;
+                else numberOFB++;
+            }
+            if (numberOFA > numberOFB) Console.WriteLine("Anton");
+            else if (numberOFA < numberOFB) Console.WriteLine("Danik");
+            else Console.WriteLine("Friendship");
+        }
+    }
+}
---
+++ A. George and Accommodation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace A._George_and_Accommodation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort n = Convert.ToUInt16(Console.ReadLine()), numberOfRooms = 0;
+            for (ushort i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ');
+                if (Convert.ToInt16(data[1]) - Convert.ToInt16(data[0]) >= 2) numberOfRooms++;
+            }
+
+            Console.WriteLine(numberOfRooms);
+        }
+    }
+}
