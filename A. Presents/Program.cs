a50ccf34bd8df41ca6e0579ce7de908946169501
ShadyElia <shady.elia@hotmail.com>
2020-08-18 00:05:03 +0200

    finish A.Presents

---
+++ A. Presents/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace A._Presents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            string[] line = Console.ReadLine().Split(' ');
+            var numbers = line.Select(int.Parse).ToList();
+
+            for (int i = 1; i <= numbers.Count; i++)
+            {
+                Console.Write(numbers.FindIndex(number => number == i) + 1);
+                if (i != numbers.Count) Console.Write(" ");
+            }
+        }
+    }
+}
