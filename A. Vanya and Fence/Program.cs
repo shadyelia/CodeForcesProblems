c8e866cf4566708002ae25d2ed9219613c21a17b
ShadyElia <shady.elia@hotmail.com>
2020-09-03 18:47:03 +0200

    finish A. Vanya and Fence

---
+++ A. Vanya and Fence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A._Vanya_and_Fence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short result = 0;
+
+            string[] data = Console.ReadLine().Split(' ');
+            short l = Convert.ToInt16(data[1]);
+
+            string[] numbers = Console.ReadLine().Split(' ');
+
+            foreach (var number in numbers)
+            {
+                if (l < Convert.ToInt16(number)) result += 2;
+                else result++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
