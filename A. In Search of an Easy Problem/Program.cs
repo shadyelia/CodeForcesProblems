65432e54b5f1e5840590a8fc16f92f8f36eace6f
ShadyElia <shady.elia@hotmail.com>
2020-09-05 20:55:50 +0200

    finish 1030A - In Search of an Easy Problem

---
+++ A. In Search of an Easy Problem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace A._In_Search_of_an_Easy_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            if (Console.ReadLine().Contains("1"))
+                Console.WriteLine("HARD");
+            else Console.WriteLine("EASY");
+        }
+    }
+}
