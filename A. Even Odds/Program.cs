44bd3599918d8ae588d0de7deed4a98d57bd198b
ShadyElia <shady.elia@hotmail.com>
2020-09-05 16:14:50 +0200

    finish even odds

---
+++ A. Even Odds/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A._Even_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            long n = Convert.ToInt64(data[0]);
+            long k = Convert.ToInt64(data[1]);
+
+            if (k > (n + 1) / 2)
+                Console.WriteLine((k - (n + 1) / 2) * 2);
+            else
+                Console.WriteLine(k * 2 - 1);
+
+        }
+    }
+}
