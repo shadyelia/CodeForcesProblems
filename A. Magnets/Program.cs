2b130f91680e1d7decb40ec8ef55ca212f741907
ShadyElia <shady.elia@hotmail.com>
2020-09-05 13:16:32 +0200

    finish A. Magnets

---
+++ A. Magnets/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace A._Magnets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            string data = "", lastOne = "";
+            while (n-- > 0)
+            {
+                data = Console.ReadLine();
+                if (data != lastOne) result++;
+                lastOne = data;
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
