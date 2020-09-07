fa66035b0e884ab6e9bebbbe84518b262992e7ec
ShadyElia <shady.elia@hotmail.com>
2020-09-07 22:37:37 +0200

    finish A. Calculating Function

---
+++ A. Calculating Function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A._Calculating_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Convert.ToInt64(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine(number / 2);
+            else
+                Console.WriteLine(-(number + 1) / 2);
+
+        }
+    }
+}
