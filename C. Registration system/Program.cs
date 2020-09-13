a11c90c5ad9fb5a74a65b9dc6b50027602cac4ca
ShadyElia <shady.elia@hotmail.com>
2020-09-13 21:23:21 +0200

    finish C. Registration system

---
+++ C. Registration system/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace C._Registration_system
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Hashtable users = new Hashtable();
+
+            while (n-- > 0)
+            {
+                string name = Console.ReadLine();
+                if (users.Contains(name))
+                {
+                    int number = Convert.ToInt32(users[name]) + 1;
+                    users[name] = number;
+                    Console.WriteLine(name + number.ToString());
+                }
+                else
+                {
+                    users.Add(name, 0);
+                    Console.WriteLine("OK");
+                }
+            }
+        }
+    }
+}
