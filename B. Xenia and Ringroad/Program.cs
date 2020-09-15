8e9443f35a761f3b8de51c0c2c9e5e1a6f161f4d
ShadyElia <shady.elia@hotmail.com>
2020-09-15 23:58:38 +0200

    finish B. Xenia and Ringroad

---
+++ B. Xenia and Ringroad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace B._Xenia_and_Ringroad
+{
+    class Program
+    {
+        static void Main()
+        {
+            long n = Convert.ToInt32(Console.ReadLine().Split(' ')[0]);
+            long steps = 0, lastNumber = 1;
+
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse);
+            foreach (long currentNumber in numbers)
+            {
+                if (currentNumber == lastNumber) steps += 0;
+                else if (currentNumber > lastNumber) steps += (currentNumber - lastNumber);
+                else steps += ((n + currentNumber) - lastNumber);
+                lastNumber = currentNumber;
+            }
+            Console.WriteLine(steps);
+        }
+    }
+}
