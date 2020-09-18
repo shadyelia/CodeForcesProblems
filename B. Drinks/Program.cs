b50e3a1d0108ee681b168edc4398b74df6aeb584
ShadyElia <shady.elia@hotmail.com>
2020-09-18 20:08:33 +0200

    finish B. Drinks

---
+++ B. Drinks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace B._Drinks
+{
+    class Program
+    {
+        static void Main()
+        {
+            short n = Convert.ToInt16(Console.ReadLine());
+            double result = Console.ReadLine().Split(' ').Select(double.Parse).Sum() / n;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            super s1 = new child1();
+            super s2 = new child2();
+
+            s1.Do("");
+            s2.Do("");
+        }
+
+        
+    }
+
+    class super
+    {
+        public virtual void Do(string message)
+        {
+            Console.WriteLine("Hello World Super!");
+        }
+    }
+
+    class child1 : super
+    {
+        public override void Do(string message)
+        {
+            Console.WriteLine("Hello World c1!");
+            base.Do("Hello World c1!");
+        }
+    }
+
+    class child2 : super
+    {
+        public void Do()
+        {
+            Console.WriteLine("Hello World c2!");
+        }
+    }
+}
