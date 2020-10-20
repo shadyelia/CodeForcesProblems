c1c243f4c21b2d7082fb0c68296dd862b859abe1
ShadyElia <shady.elia@hotmail.com>
2020-10-20 23:53:57 +0200

    finish A. New Year Candles

---
+++ A. Dragons/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A._Dragons
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            long myPower = Convert.ToInt64(data[0]);
+            long n = Convert.ToInt64(data[1]);
+            List<Dragon> dargons = new List<Dragon>();
+            long maxPower = 0, totalBounes = myPower;
+
+            while (n-- > 0)
+            {
+                data = Console.ReadLine().Split(' ');
+                long dargonPower = Convert.ToInt64(data[0]);
+                dargons.Add(new Dragon { DargonPower = dargonPower, DargonBouns = Convert.ToInt64(data[1]) });
+                if (maxPower < dargonPower) maxPower = dargonPower;
+                totalBounes += Convert.ToInt64(data[1]);
+            }
+
+            if (totalBounes < maxPower)
+            {
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+
+            dargons = dargons.OrderBy(dargon => dargon.DargonPower)
+                        .ThenBy(dargon => dargon.DargonBouns).ToList();
+
+            for (int i = 0; i < dargons.Count; i++)
+            {
+                if (myPower <= dargons[i].DargonPower)
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+                else myPower += dargons[i].DargonBouns;
+            }
+            Console.WriteLine("YES");
+        }
+    }
+
+    class Dragon
+    {
+        public long DargonPower { get; set; }
+        public long DargonBouns { get; set; }
+    }
+}
---
+++ A. New Year Candles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A._New_Year_Candles
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(data[0]);
+            int b = Convert.ToInt32(data[1]);
+
+            int counter = a;
+
+            while (a / b > 0)
+            {
+                counter += a / b;
+                a = (a / b) + (a % b);
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
