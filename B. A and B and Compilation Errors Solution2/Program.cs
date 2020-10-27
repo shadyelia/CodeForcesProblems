1e9681c115cb883672c8321ff598638bba670cb0
ShadyElia <shady.elia@hotmail.com>
2020-10-27 22:52:59 +0200

    solve B. A and B and Compilation Errors by another solution and c++

---
+++ B. A and B and Compilation Errors Solution2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace B._A_and_B_and_Compilation_Errors_Solution2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+            long sum1 = new List<long>(Console.ReadLine().Split(' ').Select(long.Parse)).Sum();
+            long sum2 = new List<long>(Console.ReadLine().Split(' ').Select(long.Parse)).Sum();
+            Console.WriteLine(sum1 - sum2);
+            sum1 = new List<long>(Console.ReadLine().Split(' ').Select(long.Parse)).Sum();
+            Console.WriteLine(sum2 - sum1);
+        }
+    }
+}
